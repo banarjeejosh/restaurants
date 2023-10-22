cf6d36c8b4a64cd02af9ef317b508c8e470637b7
Joshio Banarjee <banarjeejosh@gmail.com>
2023-10-22 10:55:46 +0600

    first push

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,171 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import React, { useState, useRef, useEffect, useCallback } from "react";
+import { gsap } from "gsap";
+import { FunctionComponent } from "react";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+// types
+interface MenuItem {
+  id: string;
+  name: string;
+  image: string;
+  link: string;
+  itemsCount: number;
+  color?: string;
 }
+// restruant menu items
+const menuItems: MenuItem[] = [
+  {
+    id: "1",
+    name: "Burger",
+    image: "/burger.png",
+    link: "/burger",
+    itemsCount: 10,
+    color: "red",
+  },
+  {
+    id: "2",
+    name: "Pizza",
+    image: "/burger.png",
+    link: "/pizza",
+    itemsCount: 10,
+    color: "blue",
+  },
+  {
+    id: "3",
+    name: "Ice Cream",
+    image: "/burger.png",
+    link: "/ice-cream",
+    itemsCount: 10,
+    color: "green",
+  },
+  {
+    id: "4",
+    name: "Drinks",
+    image: "/burger.png",
+    link: "/drinks",
+    itemsCount: 3,
+    color: "yellow",
+  },
+  {
+    id: "5",
+    name: "Drinks",
+    image: "/burger.png",
+    link: "/drinks",
+    itemsCount: 5,
+    color: "purple",
+  },
+  {
+    id: "6",
+    name: "Drinks",
+    image: "/burger.png",
+    link: "/drinks",
+    itemsCount: 10,
+    color: "pink",
+  },
+];
+
+const Home: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const ITEM_WIDTH = 300; // This should be the width of your item including any margin or padding
+  const CLONE_COUNT = 2; // Number of items to clone at the end and beginning
+
+  // Clone the first and last items to give the illusion of an infinite carousel
+  const itemsWithClones = [
+    ...menuItems.slice(-CLONE_COUNT),
+    ...menuItems,
+    ...menuItems.slice(0, CLONE_COUNT),
+  ];
+
+  // On mount, set the initial position to the first "real" item, not a clone
+  useEffect(() => {
+    if (containerRef.current) {
+      gsap.set(containerRef.current, {
+        x: -ITEM_WIDTH * CLONE_COUNT, // because we added CLONE_COUNT items before the real items
+      });
+    }
+  }, []);
+
+  const handleNextClick = () => {
+    if (containerRef.current) {
+      gsap.to(containerRef.current, {
+        x: "-=" + ITEM_WIDTH,
+        modifiers: {
+          x: (x) => {
+            // When we reach the end, loop back to the beginning seamlessly
+            const newX = parseFloat(x);
+            if (newX <= -ITEM_WIDTH * (menuItems.length + CLONE_COUNT)) {
+              return -ITEM_WIDTH * CLONE_COUNT + "px";
+            }
+            return x;
+          },
+        },
+      });
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (containerRef.current) {
+      gsap.to(containerRef.current, {
+        x: "+=" + ITEM_WIDTH,
+        modifiers: {
+          x: (x) => {
+            // When we reach the beginning, loop back to the end seamlessly
+            const newX = parseFloat(x);
+            if (newX >= -ITEM_WIDTH) {
+              return -ITEM_WIDTH * menuItems.length + "px";
+            }
+            return x;
+          },
+        },
+      });
+    }
+  };
+
+  return (
+    <div style={{ overflow: "hidden", position: "relative", width: "900px" }}>
+      {" "}
+      {/* Adjust the wrapper width as needed */}
+      <div
+        ref={containerRef}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          willChange: "transform",
+          position: "relative",
+        }}
+      >
+        {itemsWithClones.map((item, index) => (
+          <div key={index} style={{ minWidth: `${ITEM_WIDTH}px` }}>
+            <div style={{ backgroundColor: item.color, padding: "20px" }}>
+              <Image
+                src={item.image}
+                width={200}
+                height={200}
+                alt={item.name}
+              />
+              <h2>{item.name}</h2>
+              <p>{`${item.itemsCount} items`}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={handlePrevClick}
+        style={{ position: "absolute", top: "50%", left: "0", zIndex: 2 }}
+      >
+        Prev
+      </button>
+      <button
+        onClick={handleNextClick}
+        style={{ position: "absolute", top: "50%", right: "0", zIndex: 2 }}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Home;
