1e9187e3834fe53790599cee09d1379c2395e712
Joshio Banarjee <banarjeejosh@gmail.com>
2023-10-29 16:12:46 +0600

    issue fixed

--- app/component/CircleComponent.tsx
+++ app/component/CircleComponent.tsx
@@ -31,23 +31,37 @@ const CircleComponent = ({ items }: any) => {
   const handleClick = (index: number) => {
     if (items.length <= 1) return; // No need to handle click for one item
 
-    const totalItems = items.length;
-    const forwardSteps = (index - centerIndex + totalItems) % totalItems;
-    const backwardSteps = (centerIndex - index + totalItems) % totalItems;
-    // const forwardSteps = 2;
-    // const backwardSteps = 1;
+    if (items.length <= 4) {
+      const totalItems = items.length;
+      const forwardSteps = (index - centerIndex + totalItems) % totalItems;
+      const backwardSteps = (centerIndex - index + totalItems) % totalItems;
 
-    const moveForward = forwardSteps <= backwardSteps;
-    const stepsToMove = moveForward ? forwardSteps : backwardSteps;
-    // const stepSize = moveForward ? 1 : -1;
-    const stepSize = moveForward ? 1 : -1;
+      const moveForward = forwardSteps <= backwardSteps;
+      const stepsToMove = moveForward ? forwardSteps : backwardSteps;
+      const stepSize = moveForward ? 1 : -1;
 
-    const newQueue = Array.from({ length: stepsToMove }, (_, i) => {
-      const nextStep = centerIndex + stepSize * (i + 1);
-      return (nextStep + totalItems) % totalItems; // Ensuring we don't go out of bounds
-    });
+      const newQueue = Array.from({ length: stepsToMove }, (_, i) => {
+        const nextStep = centerIndex + stepSize * (i + 1);
+        return (nextStep + totalItems) % totalItems;
+      });
 
-    setAnimationQueue(newQueue);
+      setAnimationQueue(newQueue);
+    } else {
+      const totalItems = items.length;
+      const forwardSteps = (index - centerIndex + totalItems) % totalItems;
+      const backwardSteps = (centerIndex - index + totalItems) % totalItems;
+
+      const moveForward = forwardSteps <= backwardSteps;
+      const stepsToMove = moveForward ? forwardSteps : backwardSteps;
+      const stepSize = moveForward ? 1 : -1;
+
+      const newQueue = Array.from({ length: stepsToMove }, (_, i) => {
+        const nextStep = centerIndex + stepSize * (i + 1);
+        return (nextStep + totalItems) % totalItems; // Ensuring we don't go out of bounds
+      });
+
+      setAnimationQueue(newQueue);
+    }
   };
 
   const handlePointerDown = (event: any) => {
@@ -70,26 +84,20 @@ const CircleComponent = ({ items }: any) => {
     initialX.current = null;
   };
 
-  //   const getVisibleItems = () => {
-  //     const visibleItems = [];
-
-  //     if (items.length === 1) {
-  //       return [items[0]]; // If only one item, it's the only one visible
-  //     }
-
-  //     const totalVisibleItems = items.length < 5 ? items.length : 5; // Do not attempt to show more items than exist
-  //     const halfWay = Math.floor(totalVisibleItems / 2);
-
-  //     for (let i = -halfWay; i <= halfWay; i++) {
-  //       const currentIndex = (centerIndex + i + items.length) % items.length;
-  //       visibleItems.push(items[currentIndex]);
-  //     }
-  //     return visibleItems;
-  //   };
-
   const getVisibleItems = () => {
     const visibleItems = [];
-    if (items.length >= 5) {
+    if (items.length === 1) {
+      visibleItems.push(items[0]);
+      return visibleItems; // If only one item, it's the only one visible
+    }
+    if (items.length > 1 && items.length <= 4) {
+      for (let i = -1; i <= 1; i++) {
+        // Display three items centered around the centerIndex
+        const currentIndex = (centerIndex + i + items.length) % items.length;
+        visibleItems.push(items[currentIndex]);
+      }
+      return visibleItems;
+    } else if (items.length >= 5) {
       const totalVisibleItems = items.length < 5 ? items.length : 5; // Do not attempt to show more items than exist
       const halfWay = Math.floor(totalVisibleItems / 2);
 
@@ -120,6 +128,14 @@ const CircleComponent = ({ items }: any) => {
 
   // New function to calculate the degree of rotation
   const calculateRotationDegree = (totalItems: number, index: number) => {
+    // If there are fewer than 3 items, we adjust the positions without repeating the items.
+
+    if (totalItems <= 3) {
+      // it should be 3 item in a 45 degree circle with 1 item in the center
+      const degreePerItem = 90 / (totalItems - 1); // Distribute items in 180 degrees
+      return degreePerItem * index - 45; // Shift by -90 to start from the left
+    }
+
     // Calculate the degree based on the total visible items
     const degreePerItem = 180 / (totalItems - 1); // Distribute items in 180 degrees
     return degreePerItem * index - 90; // Shift by -90 to start from the left
@@ -174,43 +190,43 @@ const CircleComponent = ({ items }: any) => {
                 </div>
               </div>
             );
-          } else if (items.length <= 3 && items.length > 1) {
+          } else if (items.length <= 4 && items.length > 1) {
+            if (item === null) return <></>;
             const totalVisibleItems = getVisibleItems().length;
 
             const rotationDegree = calculateRotationDegree(
               totalVisibleItems,
               index
             );
-
+            // debugger;
             return (
               <div
                 key={item.id}
                 className={styles.carouselItem}
                 style={{
-                  transform: `rotate(${rotationDegree}deg) translateY(-150px)`,
+                  transform: `rotate(${
+                    (index - 1) * 45
+                  }deg) translateY(-150px)`,
                   backgroundColor: "transparent",
-                  opacity:
-                    index === Math.floor(totalVisibleItems / 2) ? 1 : 0.7, // adjust as needed
-                  zIndex: index === Math.floor(totalVisibleItems / 2) ? 3 : 2, // adjust as needed
+                  opacity: index === 1 ? 1 : 0.7,
+                  zIndex: index === 1 ? 3 : 2,
                 }}
                 onClick={() =>
                   handleClick(
-                    (centerIndex +
-                      index -
-                      Math.floor(totalVisibleItems / 2) +
-                      items.length) %
-                      items.length
+                    (centerIndex + index - 1 + items.length) % items.length
                   )
                 }
               >
                 <div className="flex flex-col justify-center align-middle items-center">
                   <MenuItem item={item} />
-
                   <CirclePointer fillColor={item.color} />
                 </div>
               </div>
             );
           } else if (items.length <= 5 && items.length > 3) {
+            console.log(
+              (centerIndex + index - 2 + items.length) % items.length
+            );
             return (
               <div
                 key={item.id}
@@ -251,6 +267,7 @@ const CircleComponent = ({ items }: any) => {
               </div>
             );
           }
+
           return (
             <div
               key={item.id}
